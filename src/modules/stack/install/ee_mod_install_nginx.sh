8cc14f2bc38d2450b229e714a608b2db2c7cc83b
root <amit.dhanani@yahoo.com>
2014-07-19 15:18:21 +0530

    ee mod install nginx changed

--- src/modules/stack/install/ee_mod_install_nginx.sh
+++ src/modules/stack/install/ee_mod_install_nginx.sh
@@ -1,8 +1,9 @@
 # Install nginx package
 
 function ee_mod_install_nginx()
-{
-	ee_lib_echo "Installing $EE_NGINX_PACKAGE, please wait..."
-	$EE_APT_GET install $EE_NGINX_PACKAGE \
-	|| ee_lib_error "Unable to install $EE_NGINX_PACKAGE, exit status = " $?
+{       EE1_NGINX_PACKAGE="nginx"; 
+	ee_lib_echo "Installing $EE1_NGINX_PACKAGE, please wait..."
+        
+	$EE_APT_GET install --force-yes $EE1_NGINX_PACKAGE \
+	|| ee_lib_error "Unable to install $EE1_NGINX_PACKAGE, exit status = " $?
 }
